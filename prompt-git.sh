1ab2fe28d0bb05df312a78ad5e589c2ad9e9dc81
Alexander Mayorov <alexander.majorov@gmail.com>
2021-08-11 07:46:20 +0300

    Update prompt-git.sh

--- prompt-git.sh
+++ prompt-git.sh
@@ -1,24 +1,40 @@
+
 find_git_branch()
 {
-  local branch
-  if branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null); then
-    if [[ "$branch" == "HEAD" ]]; then
-      branch='detached*'
+    local branch
+    if branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null)
+    then
+        if [[ "$branch" == "HEAD" ]]
+        then
+            branch='detached*'
+        fi
+        git_branch="($branch)"
+    else
+        git_branch=""
     fi
-    git_branch="($branch)"
-  else
-    git_branch=""
-  fi
 }
+
 
 find_git_dirty()
 {
-  local status=$(git status --porcelain 2> /dev/null)
-  if [[ "$status" != "" ]]; then
-    git_dirty='*'
-  else
-    git_dirty=''
-  fi
+    local status=$(git status --porcelain 2> /dev/null)
+    if [[ "$status" != "" ]]
+    then
+        git_dirty='*'
+    else
+        git_dirty=''
+    fi
 }
 
-PROMPT_COMMAND="find_git_branch; find_git_dirty; $PROMPT_COMMAND"
+
+#
+# Install
+#
+#if -z `echo $PROMPT_COMMAND | grep 'find_git_branch1'`
+if ! grep -q 'find_git_branch' <<< $PROMPT_COMMAND
+then
+    PROMPT_COMMAND="find_git_branch; find_git_dirty; $PROMPT_COMMAND"
+fi
+
+
+# EOF #
