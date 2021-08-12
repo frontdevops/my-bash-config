5cec8009b6d8108c139014a4e7326be4b3f82f1e
Alexander Mayorov <alexander.majorov@gmail.com>
2021-08-12 12:26:21 +0300

    Update macos.sh

--- macos.sh
+++ macos.sh
@@ -1,9 +1,31 @@
-chbg()
+
+function chbg()
 {
     osascript /Users/mayorov/Library/Mobile\ Documents/com~apple~ScriptEditor2/Documents/bgImgIterm.scpt $@
 }
 
+function new()
+{
+    if [[ "$TERM_PROGRAM" == "iTerm.app" ]]
+    then
+        app_name="iTerm"
+    else
+        app_name="Terminal"
+    fi
+    
+    if [[ $# -eq 0 ]]
+    then
+        open -a "$app_name" "$PWD"
+    else
+        open -a "$app_name" "$@"
+    fi
+}
 
+
+
+#
+# -----
+#
 if [ $(id -u) = 0 ]
 then
     chbg "kali4.jpg"
@@ -11,3 +33,11 @@ else
     chbg "kali5.jpg"
 fi
 
+
+function logout()
+{
+    chbg "kali5.jpg"
+}
+
+
+trap logout EXIT
